9d6988ce808659953f96ee80af661395abac20ec
yuzhanglong <loveyzl1123@gmail.com>
2022-11-20 15:36:08 +0800

    fix: fix eslint problems

--- packages/web-client/pages/api/hello.ts
+++ packages/web-client/pages/api/hello.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
+interface Data {
   name: string
 }
 
@@ -9,5 +9,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'John Doe' });
 }
